bcb7276cd335cd31fc95d89b9476a0887d109dd3
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-01-20 08:00:23 -0600

    Started work

--- 3D_Graphics_Engine/3D_Graphics_Engine.cpp
+++ 3D_Graphics_Engine/3D_Graphics_Engine.cpp
@@ -1,20 +1,94 @@
-// 3D_Graphics_Engine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "olcConsoleGameEngine.h"
+using namespace std;
 
-#include <iostream>
+struct vec3D
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3D p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
+struct mat4x4
+{
+	float m[4][4] = { 0 };
+};
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public: 
+	olcEngine3D() {
+		m_sAppName = L"3D Demo";
+	}
+
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+
+
+public:
+	bool OnUserCreate() override
+	{
+		meshCube.tris = {
+
+		// SOUTH
+			{ 0.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,		1.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f },
+
+		// EAST
+			{ 1.0f, 0.0f, 0.0f,		1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f, 1.0f },
+		// NORTH
+			{ 1.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,	0.0f, 0.0f, 1.0f },
+		// WEST
+			{ 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f },
+		// TOP
+			{ 0.0f, 1.0f, 0.0f,		0.0f, 1.0f, 1.0f,	1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 1.0f, 0.0f },
+		//BOTTOM
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f },
+		};
+
+		// Projection Matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		mat
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Draw Triangles
+		for (auto tri : meshCube.tris) {
+
+		}
+		return true;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	olcEngine3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+		demo.Start();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
